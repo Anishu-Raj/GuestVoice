d851c8c8a3f4354708c4569d502cf12d1a5c7d83
Anishu-Raj <anishuraj0208@gmail.com>
2026-06-13 19:06:30 +0530

    feat: complete week 2 frontend skeleton

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,29 +2,31 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-blue-600">
-        GuestVoice
-      </h1>
+    <nav className="flex flex-col md:flex-row justify-between items-center px-8 py-4 bg-gray-900">
 
-      <div className="space-x-6">
-        <Link to="/" className="hover:text-blue-600">
-          Home
-        </Link>
+  <h1 className="text-2xl font-bold text-blue-600">
+    GuestVoice
+  </h1>
 
-        <Link to="/about" className="hover:text-blue-600">
-          About
-        </Link>
+  <div className="flex flex-col md:flex-row gap-3 md:gap-6 text-white mt-4 md:mt-0">
+    <Link to="/" className="hover:text-blue-600">
+      Home
+    </Link>
 
-        <Link to="/dashboard" className="hover:text-blue-600">
-          Dashboard
-        </Link>
+    <Link to="/about" className="hover:text-blue-600">
+      About
+    </Link>
 
-        <Link to="/login" className="hover:text-blue-600">
-          Login
-        </Link>
-      </div>
-    </nav>
+    <Link to="/dashboard" className="hover:text-blue-600">
+      Dashboard
+    </Link>
+
+    <Link to="/login" className="hover:text-blue-600">
+      Login
+    </Link>
+  </div>
+
+</nav>
   );
 }
 
