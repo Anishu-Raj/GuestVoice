850a5de2ae6235d4b42b06f40cc5e5685e43e460
Anishu-Raj <anishuraj0208@gmail.com>
2026-06-27 19:01:16 +0530

    feat: add sample dataset

---
+++ backend/data/reviews.js
@@ -0,0 +1,27 @@
+const reviews = [
+  {
+    id: 1,
+    guest: "Rahul",
+    review: "Room was very clean",
+    rating: 5,
+    sentiment: "Positive"
+  },
+
+  {
+    id: 2,
+    guest: "Priya",
+    review: "Food quality was average",
+    rating: 3,
+    sentiment: "Neutral"
+  },
+
+  {
+    id: 3,
+    guest: "Aman",
+    review: "WiFi was poor",
+    rating: 2,
+    sentiment: "Negative"
+  }
+];
+
+module.exports = reviews;
