b2279b7d48317c2212b3bc51ea577585ce537e96
Anishu-Raj <anishuraj0208@gmail.com>
2026-07-04 17:03:13 +0530

    Week 5: Setup backend structure for MongoDB integration

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import reviewRoutes from "./routes/reviewRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`🚀 Server Running on Port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("❌ MongoDB Connection Failed");
+    console.log(err.message);
+  });
+
 app.use("/api/reviews", reviewRoutes);
-
-app.listen(5000, () => {
-
-    console.log("Server Running");
-
-});
